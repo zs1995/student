3ab6dc9e4d20002bc1a9537858246131b03f154c
1239185705@qq.com <123456789@qq.com>
2018-03-01 11:20:33 +0800

    struct.cpp commit

---
+++ struct.cpp
@@ -0,0 +1,31 @@
+//结构体对其以及设置其对齐规则
+
+#include <stdio.h>
+#include <stdlib.h>
+#pragma pack(1)
+typedef struct A
+{
+	int b;
+	double c;
+	char d;
+}n,*a;
+#pragma pack()
+
+typedef union X
+{
+	int y;
+	double z;
+	char u;
+}x;
+
+int main(int argc,char **argv)
+{
+	a m  = (struct A*)malloc(sizeof(struct A*));
+	//memcpy(m,0,sizeof(m));
+	m->b = 5;
+	m->c = 9.0;
+	printf("%d--%d\n",sizeof(n),m->b);
+	printf("%d\n",sizeof(X));
+	getchar();
+	return 0;
+}
